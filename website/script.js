8d916ebebde3ba2aa69afc34cb1ab890eaaa87af
TlunSter <139344410+TlunSter@users.noreply.github.com>
2023-07-12 12:35:02 +0200

    Add files via upload

---
+++ website/script.js
@@ -0,0 +1,167 @@
+const menuButton = document.getElementById('menuButton');
+const menuOverlay = document.getElementById('menuOverlay');
+const rightOverlay = document.getElementById('rightOverlay');
+const closeButton = document.getElementById('closeButton');
+const inmenuButton1 = document.getElementById('button1');
+const inmenuButton2 = document.getElementById('button2');
+const inmenuButton3 = document.getElementById('button3');
+const inmenuButton4 = document.getElementById('button4');
+const inmenuButton5 = document.getElementById('button5');
+const inmenuButton6 = document.getElementById('button6');
+const inmenuButton7 = document.getElementById('button7');
+const inmenuButton8 = document.getElementById('button8');
+const inmenuButton9 = document.getElementById('button9');
+const inmenuButton10 = document.getElementById('button10');
+
+menuButton.addEventListener('click', function() {
+    menuOverlay.style.display = 'block';
+    rightOverlay.style.display = 'block';
+    
+})
+
+closeButton.addEventListener('click', function() {
+    menuOverlay.style.display = 'none';
+    rightOverlay.style.display = 'none';
+})
+
+inmenuButton1.addEventListener('mouseover', function(){
+    inmenuButton1.style.color = 'cyan';
+    inmenuButton1.style.borderColor = 'cyan';
+})
+
+inmenuButton1.addEventListener('mouseout', function(){
+    inmenuButton1.style.color = 'white';
+    inmenuButton1.style.borderColor = 'white';
+})
+
+inmenuButton2.addEventListener('mouseover', function(){
+    inmenuButton2.style.color = 'cyan';
+    inmenuButton2.style.borderColor = 'cyan';
+})
+
+inmenuButton2.addEventListener('mouseout', function(){
+    inmenuButton2.style.color = 'white';
+    inmenuButton2.style.borderColor = 'white';
+})
+
+inmenuButton3.addEventListener('mouseover', function(){
+    inmenuButton3.style.color = 'cyan';
+    inmenuButton3.style.borderColor = 'cyan';
+})
+
+inmenuButton3.addEventListener('mouseout', function(){
+    inmenuButton3.style.color = 'white';
+    inmenuButton3.style.borderColor = 'white';
+})
+
+inmenuButton4.addEventListener('mouseover', function(){
+    inmenuButton4.style.color = 'cyan';
+    inmenuButton4.style.borderColor = 'cyan';
+})
+
+inmenuButton4.addEventListener('mouseout', function(){
+    inmenuButton4.style.color = 'white';
+    inmenuButton4.style.borderColor = 'white';
+})
+
+inmenuButton5.addEventListener('mouseover', function(){
+    inmenuButton5.style.color = 'cyan';
+    inmenuButton5.style.borderColor = 'cyan';
+})
+
+inmenuButton5.addEventListener('mouseout', function(){
+    inmenuButton5.style.color = 'white';
+    inmenuButton5.style.borderColor = 'white';
+})
+
+inmenuButton6.addEventListener('mouseover', function(){
+    inmenuButton6.style.color = 'cyan';
+    inmenuButton6.style.borderColor = 'cyan';
+})
+
+inmenuButton6.addEventListener('mouseout', function(){
+    inmenuButton6.style.color = 'white';
+    inmenuButton6.style.borderColor = 'white';
+})
+
+inmenuButton7.addEventListener('mouseover', function(){
+    inmenuButton7.style.color = 'cyan';
+    inmenuButton7.style.borderColor = 'cyan';
+})
+
+inmenuButton7.addEventListener('mouseout', function(){
+    inmenuButton7.style.color = 'white';
+    inmenuButton7.style.borderColor = 'white';
+})
+
+inmenuButton8.addEventListener('mouseover', function(){
+    inmenuButton8.style.color = 'cyan';
+    inmenuButton8.style.borderColor = 'cyan';
+})
+
+inmenuButton8.addEventListener('mouseout', function(){
+    inmenuButton8.style.color = 'white';
+    inmenuButton8.style.borderColor = 'white';
+})
+
+inmenuButton9.addEventListener('mouseover', function(){
+    inmenuButton9.style.color = 'cyan';
+    inmenuButton9.style.borderColor = 'cyan';
+})
+
+inmenuButton9.addEventListener('mouseout', function(){
+    inmenuButton9.style.color = 'white';
+    inmenuButton9.style.borderColor = 'white';
+})
+
+inmenuButton10.addEventListener('mouseover', function(){
+    inmenuButton10.style.color = 'cyan';
+    inmenuButton10.style.borderColor = 'cyan';
+})
+
+inmenuButton10.addEventListener('mouseout', function(){
+    inmenuButton10.style.color = 'white';
+    inmenuButton10.style.borderColor = 'white';
+})
+
+inmenuButton1.addEventListener('click', function(){
+    location.href = "Homepage.html"
+})
+
+inmenuButton2.addEventListener('click', function(){
+    location.href = "Button2.html"
+})
+inmenuButton3.addEventListener('click', function(){
+    location.href = "Button3.html"
+})
+
+inmenuButton4.addEventListener('click', function(){
+    location.href = "Button4.html"
+})
+
+inmenuButton5.addEventListener('click', function(){
+    location.href = "Button5.html"
+})
+inmenuButton6.addEventListener('click', function(){
+    location.href = "Button6.html"
+})
+
+inmenuButton7.addEventListener('click', function(){
+    location.href = "Button7.html"
+})
+
+inmenuButton8.addEventListener('click', function(){
+    location.href = "Button8.html"
+})
+
+inmenuButton9.addEventListener('click', function(){
+    location.href = "Button9.html"
+})
+
+inmenuButton10.addEventListener('click', function(){
+    location.href = "about.html"
+})
+
+
+
+
